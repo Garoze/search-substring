d5b5ea8d7ea6feabf3fcc0d7a75f0711f9be51e5
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-11-06 22:13:40 -0300

    feat: add automata header and implementation file

---
+++ src/automata.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "automata.h"
+
+Automata* Automata_construct(Pattern* pattern)
+{
+    Automata* automata = calloc(1, sizeof(Automata));
+    automata->size = pattern->size;
+    automata->data = calloc(pattern->size + 1, sizeof(*automata->data));
+
+    automata->data[0][pattern->pattern[0] - 'a'] = 1;
+
+    for (int i = 1; i < pattern->size; ++i)
+    {
+        int prev = pattern->table[i - 1];
+
+        for (int j = 0; j < 2; ++j)
+        {
+            automata->data[i][j] = automata->data[prev][j];
+        }
+
+        automata->data[i][pattern->pattern[i] - 'a'] = i + 1;
+    }
+
+    for (int i = 0; i < 2; ++i)
+    {
+        automata->data[pattern->size][i] = pattern->size;
+    }
+
+    return automata;
+}
+
+int Automata_validate(Automata* automata, char* string)
+{
+    int state = 0;
+    int string_len = strlen(string);
+
+    for (int i = 0; i < string_len; ++i)
+    {
+        if (state == automata->size)
+            return 1;
+
+        state = automata->data[state][string[i] - 'a'];
+    }
+
+    return (state == automata->size) ? 1 : 0;
+}
+
+void Automata_print(Automata* automata)
+{
+    for (int i = 0; i < automata->size + 1; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            printf("automata[%d][%d]: %d\n", i, j, automata->data[i][j]);
+        }
+    }
+}
+
+void Automata_free(Automata* automata)
+{
+    free(automata->data);
+    free(automata);
+}
---
+++ src/automata.h
@@ -0,0 +1,18 @@
+#ifndef _AUTOMATA_H
+#define _AUTOMATA_H
+
+#include "pattern.h"
+
+struct __automata_t
+{
+    int size;
+    int (*data)[2];
+};
+typedef struct __automata_t Automata;
+
+Automata* Automata_construct(Pattern* pattern);
+int Automata_validate(Automata* automata, char* string);
+void Automata_print(Automata* automata);
+void Automata_free(Automata* automata);
+
+#endif // _AUTOMATA_H
