d2f1ab20b865848ab8609ef91325e5b0be4f6709
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-11-07 11:04:27 -0300

    refactore: better the tests and added a debug flag

--- src/main.c
+++ src/main.c
@@ -1,28 +1,37 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "automata.h"
 #include "pattern.h"
 
+#define DEBUG 1
+
 void test_search_substring()
 {
     char* substring = "aaba";
 
-    Pattern* pattern = Pattern_build(substring);
-    Automata* automata = Automata_construct(pattern);
+    Automata* automata = Automata_build(substring);
 
-    printf("%d\n", Automata_validate(automata, "bbabb"));
-    printf("%d\n", Automata_validate(automata, "abbabaabbbbaba"));
-    printf("%d\n", Automata_validate(automata, "abaaaaaabab"));
-    printf("%d\n", Automata_validate(automata, "aaaa"));
-    printf("%d\n", Automata_validate(automata, "abba"));
-    printf("%d\n", Automata_validate(automata, "abbb"));
-    printf("%d\n", Automata_validate(automata, "abba"));
-    printf("%d\n", Automata_validate(automata, "baabaab"));
-    printf("%d\n", Automata_validate(automata, "aaba"));
-    printf("%d\n", Automata_validate(automata, "aaabbbbab"));
+#if DEBUG
+    printf("DEBUG: automata -> `%s`\n", substring);
+    Automata_print(automata);
+    printf("\n");
+#endif
 
-    Pattern_free(pattern);
+    assert(Automata_validate(automata, "bbabb") == 0);
+    assert(Automata_validate(automata, "abbabaabbbbaba") == 0);
+    assert(Automata_validate(automata, "abaaaaaabab") == 1);
+    assert(Automata_validate(automata, "aaaa") == 0);
+    assert(Automata_validate(automata, "abba") == 0);
+    assert(Automata_validate(automata, "abbb") == 0);
+    assert(Automata_validate(automata, "abba") == 0);
+    assert(Automata_validate(automata, "baabaab") == 1);
+    assert(Automata_validate(automata, "aaba") == 1);
+    assert(Automata_validate(automata, "aaabbbbab") == 0);
+
     Automata_free(automata);
+
+    printf("All tests from `test_search_substring` passed!\n");
 }
 
 int main(void)
